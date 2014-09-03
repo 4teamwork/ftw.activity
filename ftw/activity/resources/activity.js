812627b7cfbfe2de1efffae90c65563097d54c3d
Jonas Baumann <jone@jone.ch>
2014-09-03 10:21:38 +0200

    automatically configure colorbox in activity stream.
    Use a link with class="colorboxLink" and rel="activity".

--- ftw/activity/resources/activity.js
+++ ftw/activity/resources/activity.js
@@ -1,30 +1,43 @@
-(function($) {$(document).on('ready reload', function() {
+(function($) {
+  $(document).on('ready reload', function() {
+    var events = $('.activity .events');
+    if(events.length === 0) {
+      return;
+    }
 
-  var events = $('.activity .events');
-  if(events.length === 0) {
-    return;
-  }
+    var more = $('<a />').
+        attr('href', '#').
+        addClass('more-button').
+        text(events.data('more-label')).
+        insertAfter(events);
 
-  var more = $('<a />').
-      attr('href', '#').
-      addClass('more-button').
-      text(events.data('more-label')).
-      insertAfter(events);
+    more.click(function(event) {
+      event.preventDefault();
 
-  more.click(function(event) {
-    event.preventDefault();
+      var last_uid = events.find('.event:last').data('uid');
+      var url = "activity/fetch?last_uid=".concat(last_uid);
 
-    var last_uid = events.find('>.event:last').data('uid');
-    var url = "activity/fetch?last_uid=".concat(last_uid);
-
-    $.get(url, function(data) {
-      if(data.trim().length) {
-        $(data).appendTo(events);
-      } else {
-        more.remove();
-      }
+      $.get(url, function(data) {
+        if(data.trim().length) {
+          var old_events = events.find('.event');
+          $(data).appendTo(events);
+          var new_events = events.find('.event').not(old_events);
+          events.trigger('activity-fetched', [new_events]);
+        } else {
+          more.remove();
+        }
+      });
     });
   });
 
-
-});})(jQuery);
+  $(document).on('ready reload activity-fetched', function() {
+    if(typeof($.fn.colorbox) !== 'undefined') {
+      $('.activity a.colorboxLink').colorbox({
+        'photo': true,
+        'current': '{current}/{total}',
+        'maxWidth': '100%',
+        'maxHeight': '100%'
+      });
+    }
+  });
+})(jQuery);
