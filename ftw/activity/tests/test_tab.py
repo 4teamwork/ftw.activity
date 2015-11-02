341882b721c37554280e862ab4d9f3180082d445
Jonas Baumann <jone@jone.ch>
2015-11-02 11:55:03 +0100

    Fix fetching within tabbedview tabs.
    Change rendered fetch URL
    
    Change the rendered fetch URL to no longer be generated from the
    request but to be built from the view name under which the activity view
    is rendered.
    This is done in order to make sure that we have the actually used
    activity view which should allow public access to /fetch in order to
    update with AJAX.
    
    Make /fetch and /raw traversable and public callable
    
    The "fetch" and "raw" methods of any activity view should be public
    callable in order to update views with AJAX.

--- ftw/activity/tests/test_tab.py
+++ ftw/activity/tests/test_tab.py
@@ -18,7 +18,14 @@ class TestActivityView(TestCase):
         setRoles(self.layer['portal'], TEST_USER_ID, ['Contributor'])
 
     @browsing
-    def test_fetch_more_events(self, browser):
+    def test_activity_fetch_url_points_to_activity_tab(self, browser):
+        browser.login().open(view='tabbed_view/listing?view_name=activity')
+        self.assertEquals(
+            'http://nohost/plone/tabbedview_view-activity/fetch',
+            browser.css('.events').first.attrib.get('data-fetch-url'))
+
+    @browsing
+    def test_render_events(self, browser):
         with freeze(datetime(2010, 1, 2)) as clock:
             create(Builder('page').titled('One'))
             clock.backward(days=1)
@@ -28,4 +35,11 @@ class TestActivityView(TestCase):
 
         browser.login().open(view='tabbedview_view-activity')
         self.assertEquals(['One', 'Two', 'Three'],
+                          map(attrgetter('title'), activity.events()))
+
+    @browsing
+    def test_fetch_is_traversable(self, browser):
+        create(Builder('page').titled('One'))
+        browser.login().open(view='tabbedview_view-activity/fetch')
+        self.assertEquals(['One'],
                           map(attrgetter('title'), activity.events()))
